f9ec213d2447b143adb914d3f31fb93f2c3b1f2b
Tamilselvan <st711120104079@jit.ac.in>
2023-08-29 17:34:52 +0530

    added change password button

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
-function keyUpFunction() {
-    const emailInput1 = document.getElementById('input-username');
-    const passwordInput = document.getElementById('input_password');
-    const emailAlert1 = document.getElementById('email-alert');
-    const passwordAlert = document.getElementById('password-alert');
-    const invalidEmail = document.getElementById('invalid-email');
-    const invalidPassword = document.getElementById('invalid-password');
+let user = [];
 
-    invalidEmail.innerHTML = "";
-    invalidPassword.innerHTML = "";
-    emailAlert1.style.display = 'none';
-    emailInput1.style.borderColor = "rgb(160, 160, 160)";
-}
 function validate() {
+
+    event.preventDefault();
+
     const emailInput = document.getElementById('input-username');
     const passwordInput = document.getElementById('input-password');
     const emailAlert = document.getElementById('email-alert');
     const passwordAlert = document.getElementById('password-alert');
     const invalidEmail = document.getElementById('invalid-email');
     const invalidPassword = document.getElementById('invalid-password');
-
-    let email = ['tamil@gmail.com','vijay@gmail.com','harish@gmail.com'];
-    let password = ['tamil','vijay','12345']
+    
+    let flag = 0;
 
     if (!emailInput.value) {
         emailAlert.style.display = 'inline';
         emailInput.style.borderColor = "red";
         invalidEmail.innerHTML = "*enter the email";
-        return;
     }
     
     else {         
@@ -35,13 +25,20 @@ function validate() {
             emailAlert.style.display = 'inline';
             emailInput.style.borderColor = "red";
             invalidEmail.innerHTML = "*enter valid email";
-            return;
         }
+
         else {
             emailAlert.style.display = 'none';
             emailInput.style.borderColor = "rgb(160, 160, 160)";
             invalidEmail.innerHTML = "";
         }
+    }
+
+    if (!passwordInput.value) {
+        passwordAlert.style.display = 'inline';
+        passwordInput.style.borderColor = "red";
+        invalidPassword.innerHTML = "*enter the password";
+        return;
     }
 
     if (passwordInput.value.length < 5) {
@@ -55,17 +52,122 @@ function validate() {
         passwordAlert.style.display = 'none';
         passwordInput.style.borderColor = "rgb(160, 160, 160)";
         invalidPassword.innerHTML = "";
+        flag = 1;
     }
     
-    let j=0;
+    const existingUsersJSON = localStorage.getItem('users');
+    const storedUsers = JSON.parse(existingUsersJSON);
 
-    for (let i=0 ; i<email.length ; i++) {
-        if(email[i]==emailInput.value && password[i] == passwordInput.value) {
-            j=1;
+    let existingUser;
+    let flag2= -1;
+    if(storedUsers) {
+        localStorage.setItem("flag2", storedUsers.length);
+    }
+    else {
+        localStorage.setItem("flag2", "0");
+    }
+    
+    for (let i in storedUsers) {
+        if(storedUsers[i].email == emailInput.value) {
+            flag2 = i;
+            localStorage.setItem("flag2", i);
+            existingUser = storedUsers.find(a => storedUsers[i].email == emailInput.value);
+        }
+    }
+
+    if (flag == 1) {
+        if(existingUser){
+            if(storedUsers[flag2].password == passwordInput.value){
+                window.location.href = "home.html";
+            }
+            else {
+                invalidPassword.innerHTML = "*enter correct password";
+                passwordAlert.style.display = 'inline';
+            }
+        }
+        else {
+            const newUser = {
+                email: emailInput.value,
+                password: passwordInput.value
+            };
+            
+            const existingUsersJSON = localStorage.getItem('users');
+            let existingUsers = [];
+            
+            if (existingUsersJSON) {
+                existingUsers = JSON.parse(existingUsersJSON);
+            }
+            
+            if (!Array.isArray(existingUsers)) {
+                existingUsers = [];
+            }
+            
+            existingUsers.push(newUser);
+            
+            const updatedUsersJSON = JSON.stringify(existingUsers);
+            localStorage.setItem('users', updatedUsersJSON);
+
             window.location.href = "home.html";
         }
     }
-    if (j==0) {
-        alert('Enter valid login credentials');
-    }
 }
+
+
+
+function makeVisible() {
+
+    let change = document.getElementById('change-password-hide');
+    change.style.visibility = 'visible';
+}
+
+function changePassword() {
+
+    const oldPassword = document.getElementById('old-password');
+    const newPassword = document.getElementById('new-password');
+    flag2 = localStorage.getItem("flag2");
+
+    let storedUsers = JSON.parse(localStorage.getItem('users')) || [];
+
+    const enteredPassword = oldPassword.value;
+
+    const matchedUser = storedUsers.find(user => storedUsers[flag2].password === enteredPassword);
+
+    if (matchedUser) {
+        storedUsers[flag2].password = newPassword.value;
+        localStorage.setItem('users', JSON.stringify(storedUsers));
+        
+        alert('Password updated successfully');
+
+        oldPassword.value = ''
+        newPassword.value = '';
+
+        let change = document.getElementById('change-password-hide');
+        change.style.visibility = 'hidden';
+    } else {
+        alert('Enter correct Password');
+    }
+
+}
+
+function keyUpFunctionMail() {
+
+    const emailInput1 = document.getElementById('input-username');
+    const emailAlert1 = document.getElementById('email-alert');
+    const invalidEmail = document.getElementById('invalid-email');
+
+    invalidEmail.innerHTML = "";
+    emailAlert1.style.display = 'none';
+    emailInput1.style.borderColor = "rgb(160, 160, 160)";
+}
+
+function keyUpFunctionPassword() {
+
+    const passwordInput = document.getElementById('input-password');
+    const passwordAlert = document.getElementById('password-alert');
+    const invalidPassword = document.getElementById('invalid-password');
+    
+    invalidPassword.innerHTML = "";
+    passwordAlert.style.display = 'none';
+    passwordInput.style.borderColor = "rgb(160, 160, 160)";
+}
+
